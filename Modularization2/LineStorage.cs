46be74e8d1a22a296ec4222ed562485c649b309b
ArthurDSNWSL <TheBolads@protonmail.com>
2026-04-27 10:06:33 -0300

    Module2 alterations

---
+++ Modularization2/LineStorage.cs
@@ -0,0 +1,22 @@
+namespace Modularization2;
+public class LineStorage
+{
+    string[] Lines;
+
+    LineStorage()
+    {
+        Lines = [];
+    }
+
+    public int Char(int line, int character, int word)
+    {
+             
+
+        return 0;
+    }
+    
+    public int Words(string line)
+    {
+       return 0; 
+    }
+}
